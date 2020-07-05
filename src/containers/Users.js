891bc240514784fcfb214bc9ee77713298c29801
Endramar <alikskyl@gmail.com>
2020-07-05 18:19:00 +0300

    production packages installed and App js file created

---
+++ src/containers/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Users extends Component {
+
+    render() {
+        return <div>
+            <h1>Users</h1>
+            <p>This is the users page!!</p>
+        </div>
+    }
+}
+
+
+export default Users;
